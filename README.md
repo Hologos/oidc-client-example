d5f1e831ff2bbb5fb36e5b2548c1f5da42219858
Jiří Málek <jiri.malek@gmail.com>
2020-04-22 13:14:01 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,9 @@
+# OIDC client example
+
+To run the app, execute:
+
+```bash
+./mvnw spring-boot:run
+```
+
+The app is available at [localhost:8080](http://localhost:8080).
---
+++ src/main/java/cz/cez/bso/oidc/MainController.java
@@ -0,0 +1,15 @@
+package cz.cez.bso.oidc;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+public class MainController {
+
+   @GetMapping("/")
+   String home(Principal user) {
+       return "Hello " + user.getName();
+   }
+}
